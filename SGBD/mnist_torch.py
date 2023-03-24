a9fda3001607aa52c7ceb29fd81931218754f638
Dario <filatrella.dario@gmail.com>
2023-03-24 21:43:31 +0100

    added merda

--- SGBD/mnist_torch.py
+++ SGBD/mnist_torch.py
@@ -13,31 +13,31 @@ from pythorch_custom import SGBD
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 3, 1)
-        self.conv2 = nn.Conv2d(16, 16, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        # self.fc1 = nn.Linear(28 ** 2, 10)
-        self.fc1 = nn.Linear(144 * 16, 32)
-        self.fc2 = nn.Linear(32, 10)
+        # self.conv1 = nn.Conv2d(1, 16, 3, 1)
+        # self.conv2 = nn.Conv2d(16, 16, 3, 1)
+        # self.dropout1 = nn.Dropout(0.25)
+        # self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(28 ** 2, 10)
+        # self.fc1 = nn.Linear(144 * 16, 32)
+        # self.fc2 = nn.Linear(32, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
+        # x = self.conv1(x)
+        # x = F.relu(x)
+        # x = self.conv2(x)
+        # x = F.relu(x)
+        # x = F.max_pool2d(x, 2)
+        # x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
+        # x = F.relu(x)
+        # x = self.dropout2(x)
+        # x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch, log_interval=None):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -46,12 +46,10 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
 
 
 def test(model, device, test_loader):
@@ -73,37 +71,20 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 
-def main():
+def main(use_mine=True):
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
-                        help='learning rate (default: 1.0)')
-    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
-                        help='Learning rate step gamma (default: 0.7)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--no-mps', action='store_true', default=False,
-                        help='disables macOS GPU training')
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='quickly check a single pass')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=False,
-                        help='For Saving the current Model')
 
-    args = parser.parse_args()
-    args.epochs = 16
+    lr = 1
+    gamma = .7
+    log_interval = 10
+    batch_size = 256
+    test_batch_size = 1000
+    epochs = 16
 
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-    use_mps = not args.no_mps and torch.backends.mps.is_available()
+    use_cuda = torch.cuda.is_available()
+    use_mps = torch.backends.mps.is_available()
 
-    torch.manual_seed(args.seed)
+    torch.manual_seed(2212)
 
     if use_cuda:
         device = torch.device("cuda")
@@ -112,8 +93,8 @@ def main():
     else:
         device = torch.device("cpu")
 
-    train_kwargs = {'batch_size': args.batch_size}
-    test_kwargs = {'batch_size': args.test_batch_size}
+    train_kwargs = {'batch_size': batch_size}
+    test_kwargs = {'batch_size': test_batch_size}
     if use_cuda:
         cuda_kwargs = {'num_workers': 1,
                        'pin_memory': True,
@@ -135,23 +116,24 @@ def main():
 
     model = Net().to(device)
 
-    # optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-    # scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    scheduler = None
-    optimizer = SGBD(model.parameters(), defaults={})
+    if use_mine:
+        scheduler = None
+        optimizer = SGBD(model.parameters(), defaults={})
+    else:
+        optimizer = optim.Adadelta(model.parameters(), lr=lr)
+        scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
 
     # summary(model, (28 ** 2,))
     summary(model, (1, 28, 28,))
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer, epoch, log_interval)
         test(model, device, test_loader)
         if scheduler is not None:
             scheduler.step()
 
-    if args.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+    torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
-    main()
+    main(True)
--- SGBD/pythorch_custom.py
+++ SGBD/pythorch_custom.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Union, Callable, Optional, List, Dict, Any
+from typing import Iterable, Union, Callable, Optional, Dict, Any
 
 import numpy as np
 import torch
@@ -14,21 +14,58 @@ class SGBD(Optimizer):
     # Init Method:
     def __init__(self, params, defaults: Dict[str, Any]):
         super().__init__(params, defaults)
+        self.isp = False
         self.state = dict()
+        self.tau = dict()
+        self.sigma = .01
+        self.probabilities = None
         for group in self.param_groups:
             for p in group['params']:
                 self.state[p] = dict(mom=torch.zeros_like(p.data))
+                self.tau[p] = 0
 
     # Step Method
     def step(self, closure: Optional[Callable[[], float]] = ...):
-        sigma = .01
-        alfa = 100000
+        # alfa = 100000
+        beta = .1
+        sb = .5
         for group in self.param_groups:
             for p in group['params']:  # iterates over layers, i.e. extra iteration on parameters
 
-                z = np.random.normal(sigma, 0.1 * sigma, p.grad.data.shape).astype(np.float32)
-                probs = 1 / (1 + torch.exp(p.grad.data * z * alfa))
+                self.tau[p] = (1 - beta) * self.tau[p] + beta * p.grad.data.std()
 
+                tau = self.tau[p]
+
+                z = np.random.normal(self.sigma, 0.1 * self.sigma, p.grad.data.shape).astype(np.float32)
+
+                alfa = np.ones(z.shape, dtype=np.float32)
+
+                m = abs(tau * z) < 1.702
+
+                # if abs(tau * z[i]) > 1.702:
+                #     alfa[i] = 1
+                alfa[m] = 1.702 / ((1.702 ** 2 - tau ** 2 * z[m] ** 2) ** .5)
+
+                # print(tau, alfa.mean(), alfa.std(), self.sigma)
+
+                scale = 10000
+                probs = 1 / (1 + torch.exp(p.grad.data * scale * z * alfa))
+                if self.probabilities is None:
+                    self.probabilities = list(probs.flatten())
+                    self.isp = False
+                else:
+                    # print(len(self.probabilities))
+                    if len(self.probabilities) > 1e6 and self.isp is False:
+                        plt.hist(self.probabilities, bins=50)
+                        plt.title(f"Probability distribution using param: {scale}")
+                        plt.xlim(0, 1)
+                        plt.show()
+                        # self.isp = True
+                        self.probabilities = []
+                    if self.isp is False:
+                        self.probabilities.extend(probs.flatten())
+
+                self.sigma = (1 - sb) * self.sigma + sb * (.01 - tau * z.mean())
                 # print(probs.mean(), probs.std())
                 # plt.hist(probs)
                 # plt.show()
@@ -38,7 +75,7 @@ class SGBD(Optimizer):
 
                 with np.nditer((probs, mask), op_flags=['readwrite']) as it:
                     for pro, mas in it:
-                        if np.random.uniform(0,1) < pro:
+                        if np.random.uniform(0, 1) < pro:
                             mas[...] = -1
                 p.data -= mask * z
                 # p.data -= 1e-3 * p.grad.data
