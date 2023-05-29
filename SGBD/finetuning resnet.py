4413b98a812fc683eebacd67dff46745f4bbdf91
Dario <filatrella.dario@gmail.com>
2023-05-29 19:16:03 +0200

    logged resnet

--- SGBD/finetuning resnet.py
+++ SGBD/finetuning resnet.py
@@ -16,11 +16,14 @@ if __name__ == '__main__':
         return mod
 
 
-    # main.main(True, std_model, dataset=DS, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
     # train_mod, opt = main.main(True, pre_trained_model, dataset=DS, write_logs=True, epochs=3, alfa_target=1 / 4)
     # gg = lambda x: train_mod
-    # main.main(False, gg, dataset=DS, write_logs=True, epochs=20, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=1, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.1, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.01, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
+    main.main(False, std_model, dataset=DS, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
+    # main.main(False, pre_trained_model, dataset=DS, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
 
     # train_mod_nopre, opt = main.main(std_model, gg, dataset=DS, write_logs=True, epochs=5, alfa_target=1 / 10)
     # gg2 = lambda x: train_mod_nopre
-    main.main(False, std_model, dataset=DS, write_logs=True, epochs=15, alfa_target=1 / 10)
+    # main.main(False, std_model, dataset=DS, write_logs=True, epochs=15, alfa_target=1 / 10)
