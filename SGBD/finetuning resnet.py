6117c1224d3bee6d73f006176a6bb6eba90f4f8f
Dario <filatrella.dario@gmail.com>
2023-06-03 14:23:45 +0200

    logged resnet

--- SGBD/finetuning resnet.py
+++ SGBD/finetuning resnet.py
@@ -4,26 +4,29 @@ from torchvision.models import resnet18, ResNet18_Weights
 import main
 
 DS = "CIFAR10"
-E = 10
+E = 15
 
 if __name__ == '__main__':
-    std_model = lambda x: resnet18(num_classes=10)
-
+    def std_model(use_cifar=True):
+        mod = resnet18(num_classes=10)
+        mod.__class__.__name__ = "Resnet non pretrained"
+        return mod
 
     def pre_trained_model(use_cifar=True):
         mod = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
         mod.fc = nn.Linear(512, 10, bias=True)
+        mod.__class__.__name__ = "Resnet pretrained"
         return mod
 
 
     # train_mod, opt = main.main(True, pre_trained_model, dataset=DS, write_logs=True, epochs=3, alfa_target=1 / 4)
     # gg = lambda x: train_mod
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=1, write_logs=True, epochs=E, alfa_target=1 / 10)
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=.1, write_logs=True, epochs=E, alfa_target=1 / 10)
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=.5, write_logs=True, epochs=E, alfa_target=1 / 10)
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=.05, write_logs=True, epochs=E, alfa_target=1 / 10)
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=.01, write_logs=True, epochs=E, alfa_target=1 / 10)
-    main.main(True, pre_trained_model, dataset=DS, global_stepsize=.001, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=1, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.1, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.5, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.05, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.01, write_logs=True, epochs=E, alfa_target=1 / 10)
+    main.main(True, std_model, dataset=DS, global_stepsize=.001, write_logs=True, epochs=E, alfa_target=1 / 10)
     # main.main(False, std_model, dataset=DS, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
     # main.main(False, pre_trained_model, dataset=DS, write_logs=True, epochs=EPOCHS, alfa_target=1 / 10)
 
--- SGBD/main.py
+++ SGBD/main.py
@@ -208,15 +208,9 @@ if __name__ == '__main__':
             print(f"{nnet.__name__} - {hit_adam=}")
 
     else:
-        # nnet = lambda use_cifar: torchvision.models.resnet18(num_classes=10)
-
-        main(True, nnet, corrected=False, extreme=False, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
-        # main(False, nnet, corrected=False, extreme=False, dataset=DS, epochs=EPOCHS, write_logs=True,
-        #      alfa_target=1 / 10)
-        # main(True, nnet, corrected=False, extreme=False, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
-
-        # nnet = lambda use_cifar: torchvision.models.resnet18(num_classes=10)
-        # nnet = models.LargeModel
-        # main(True, nnet, corrected=False, extreme=True, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 4)
-        # main(True, nnet, corrected=False, extreme=False, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
-
+        nnet = models.LargeModel
+        main(True, nnet, corrected=False, extreme=True, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
+        nnet = models.CnnMedium
+        main(True, nnet, corrected=False, extreme=True, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
+        nnet = models.LogisticReg
+        main(True, nnet, corrected=False, extreme=True, dataset=DS, epochs=EPOCHS, write_logs=True, alfa_target=1 / 10)
